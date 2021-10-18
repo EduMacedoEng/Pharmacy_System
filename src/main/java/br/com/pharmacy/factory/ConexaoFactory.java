5a7364cc7488df387ed7ac92e282f0dae5b1a597
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2021-10-18 02:55:21 -0300

    Config. Mysql database and connector. Created the classes Fornecedores and Produtos (tables into database) and ConexaoFactory (allow to connect with database)

---
+++ src/main/java/br/com/pharmacy/factory/ConexaoFactory.java
@@ -0,0 +1,29 @@
+package br.com.pharmacy.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoFactory {
+	private static final String USUARIO = "eduardo";
+	private static final String SENHA = "Eduardo01.";
+	private static final String URL = "jdbc:mysql://0.0.0.0:3306/sistema";
+	
+	public static Connection conectar() throws SQLException {
+		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+		
+		return conexao;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Connection conexao = ConexaoFactory.conectar();
+			System.out.println("Conectado com sucesso.");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Erro na conexão: " + e.getMessage());
+		}
+	}
+}
